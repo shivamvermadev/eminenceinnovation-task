56996280530f0bccb2d9ea3932658c85e49f513f
Shivam Verma <shivam.verma@redbus.com>
2024-04-28 02:48:05 +0530

    updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # eminenceinnovation-task
 
 # Please refer postman collection for the API's information.
+
+---
+
+- API endpoints
+  - ```/register``` will be used to register a new user or admin based on the role passed and can be accessed without authentication.
+  - ```/authenticate``` will be used to authenticate the user or admin, on successful authentication a JWT token will be received in response, that has role set and can be accessed without any authentication.
+  - ```/decode-jwt``` will show the few contents of jwt like username, issuedAt and expirationTime. This endpoint requires authentication and  will be only accessible to the user with admin role.
+  - ```/draw-matches``` This endpoint requires authentication and will be accessible to both user and admin. This endpoint will list all the matches that are drawn for the year passed as query param.
--- src/main/java/com/eminenceinnovation/task/service/MainService.java
+++ src/main/java/com/eminenceinnovation/task/service/MainService.java
@@ -2,12 +2,9 @@ package com.eminenceinnovation.task.service;
 
 import com.eminenceinnovation.task.model.FootballDTO;
 import com.eminenceinnovation.task.model.MatchInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,11 +18,11 @@ public class MainService {
         CompletableFuture.allOf(completableFuture).join();
 
         try {
-            if(completableFuture.isDone()) {
+            if (completableFuture.isDone()) {
                 FootballDTO footballDTO = completableFuture.get();
 
                 footballDTO.getMatchInfos().forEach(item -> {
-                    if(Objects.equals(item.getTeam1Goals(), item.getTeam2Goals())) {
+                    if (Objects.equals(item.getTeam1Goals(), item.getTeam2Goals())) {
                         matches.add(item);
                     }
                 });
@@ -42,14 +39,8 @@ public class MainService {
         return CompletableFuture.supplyAsync(() -> {
             try {
                 String urlString = "https://jsonmock.hackerrank.com/api/football_matches?year=" + year + "&page=1";
-
-                URL url = new URL(urlString);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestProperty("accept", "application/json");
-                connection.setRequestMethod("GET");
-                InputStream responseStream = connection.getInputStream();
-                ObjectMapper mapper = new ObjectMapper();
-                FootballDTO footballDTO = mapper.readValue(responseStream, FootballDTO.class);
+                RestTemplate restTemplate = new RestTemplate();
+                FootballDTO footballDTO = restTemplate.getForObject(urlString, FootballDTO.class);
                 return footballDTO;
             } catch (Exception e) {
                 e.printStackTrace();
